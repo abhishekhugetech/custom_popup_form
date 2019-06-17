2260b457bdeb25d88b4dd4035bf6e3de460cdb05
Abhishek <hritik6058@gmail.com>
2019-06-17 20:10:28 +0530

    Step 3 and 4 also added.

--- js/index.js
+++ js/index.js
@@ -18,14 +18,19 @@ $(document).ready(function () {
     });
 
     let form2Shown = false;
+    let form3Shown = false;
+    let form4Shown = false;
+
+    showPopup();
 
     // Next Button Click Listener
     $("#popup_main_continue").click(function(e){
 
         // Prevent Default behaviour until all fields are filled.
         if ($("#user_email").val().length !== 0 &&
-            $("#user_username").val().length !== 0 &&
-            $("#user_password").val().length !== 0 ){
+            $("#user_name").val().length !== 0 &&
+            $("#user_phone").val().length !== 0 &&
+            $("#user_age").val().length !== 0 ){
             // Fields are not Empty
 
             // Showing Form 2
@@ -35,27 +40,61 @@ $(document).ready(function () {
                 $("#form2").show();
                 // Increase the Progress of Form to 50%
                 $('.popup_main_form_progress_current').width('50%');
-                $("#popup_main_continue").html('Continue');
+                $("#popup_main_continue").html('Next');
             }
 
             // Check if Form2's field are filled or not
-            if ($("#user_college").val().length === 0 ||
-                $("#user_phone").val().length === 0  ){
+            if ($("#user_college").val().length === 0  ){
                 // College and Phone Fields empty, show any message if you want.
             }else {
-                // Disable the default Behaviour of form Submit
-                e.preventDefault();
-                // Increase the Progress of Form to 100%
-                $('.popup_main_form_progress_current').width('100%');
-                // Do any kind of Ajax Form Processing.
-                alert('Form Submission Completed');
+
+                // Showing Form 3
+                if (!form3Shown) {
+                    form3Shown = true;
+                    $("#form1").hide();
+                    $("#form2").hide();
+                    $("#form3").show();
+                    // Increase the Progress of Form to 75%
+                    $('.popup_main_form_progress_current').width('75%');
+                    $("#popup_main_continue").html('Continue');
+                    return
+                }
+
+                if (!$("#student").is(":checked") &&
+                    !$("#teacher").is(":checked") &&
+                    !$("#corporate").is(":checked") ) {
+                    // do something
+                    alert('Please Select a Pack');
+                } else {
+
+
+                    // Disable the default Behaviour of form Submit
+                    e.preventDefault();
+
+                    // Showing Screen 4
+                    if (!form4Shown) {
+                        form4Shown = true;
+                        $("#form1").hide();
+                        $("#form2").hide();
+                        $("#form3").hide();
+                        $("#form4").show();
+                        // Increase the Progress of Form to 100%
+                        $('.popup_main_form_progress_current').width('100%');
+                        $("#popup_main_continue").html('Continue');
+                    }
+
+                    // Do any kind of Ajax Form Processing.
+                    setTimeout(function(){ alert('Form Submission Completed'); }, 1000);
+
+                }
+
             }
 
         }else {
             // Email,UserName or password Field empty, show any message if you want.
         }
 
-
     });
 
+    showPopup();
 });
